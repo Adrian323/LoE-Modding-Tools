fefb334ffd70ea087bc6da5a339d8528423ca9cc
Adrian <andy.tache@gmail.com>
2022-03-13 23:29:44 +0200

    innitial commit

---
+++ Assets/Legends Of Ellaria Modding Tools/Example Mods/Example Mod - MoreMoney/Scripts/ModLogics_MoreMoney.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModLogics_MoreMoney : ModLogics_Base
+{
+    public int ExtraMoneyPerDayMin = 100;
+    public int ExtraMoneyPerDayMax = 300;
+
+    public override void OnIngameHourPassed()
+    {
+        
+        int amountToAdd = Random.Range(ExtraMoneyPerDayMin, ExtraMoneyPerDayMax);
+
+        GameEngine.WorldData.PlayerFactionRef.AddResources("Gold", amountToAdd);
+
+        GameEngine.Engine_Player.HEX_FPS_RTS_Manager.AddTextToQuestText("Received " + amountToAdd + " gold from an unknown donor");
+    }
+}
